a47f77892cddddb6303abddc5c40688e36d7cfe6
Xiaofan Wu <xiaofan2406@gmail.com>
2017-09-27 23:19:17 +1000

    form validations

--- example/components/Input.js
+++ example/components/Input.js
@@ -1,21 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { css } from 'emotion/react';
 
-const inputStyles = css`
-  border: none;
-  border-bottom: 1px solid #3b4045;
-  outline: none;
-  margin: 4px;
-  width: 100px;
-  text-align: center;
-  &:focus {
-    border-bottom: 1px solid #00bcd4;
+const wrapperStyle = css`
+  display: flex;
+  flex-direction: column;
+  & input {
+    border: none;
+    border-bottom: 1px solid #3b4045;
+    outline: none;
+    margin: 4px;
+    width: 100px;
+    &:focus {
+      border-bottom: 1px solid #00bcd4;
+    }
   }
 `;
 
-function Input(props) {
+const helperStyle = css`
+  height: 24px;
+  color: #3b4045;
+`;
+
+function Input({ error, feedback, ...rest }) {
   console.log('render Input');
-  return <input className={inputStyles} {...props} />;
+  return (
+    <div className={wrapperStyle}>
+      <input {...rest} />
+      <div className={helperStyle}>{error || feedback}</div>
+    </div>
+  );
 }
+
+Input.propTypes = {
+  error: PropTypes.string,
+  feedback: PropTypes.string
+};
+
+Input.defaultProps = {
+  error: '',
+  feedback: ''
+};
 
 export default Input;
--- example/components/Login.js
+++ example/components/Login.js
@@ -4,51 +4,65 @@ import { login } from 'utils/api';
 import Aux from './Aux';
 import Input from './Input';
 
-class Login extends React.Component {
-  get = () => {};
-  render() {
-    console.log('render Login');
-    return (
-      <Fiora
-        name="Login"
-        validate={async data => {
-          console.log(data);
-          const errors = {};
-          const feedbacks = {};
-          if (data.username.length < 5) {
-            errors.username = 'hum...need more than 5 characters';
-          } else {
-            feedbacks.username = 'LGTM';
-          }
-          return { errors, feedbacks };
-        }}
-        onSubmit={login}
-      >
-        {({ handleSubmit }) => {
-          console.log('render the form');
-          return (
-            <Aux>
-              <Field name="username">
-                {({ value, handleChange }) => {
-                  console.log('render username Field');
-                  return (
-                    <Input
-                      type="text"
-                      value={value}
-                      onChange={event => {
-                        handleChange(event.target.value);
-                      }}
-                    />
-                  );
-                }}
-              </Field>
-              <button onClick={handleSubmit}>Submit</button>
-            </Aux>
-          );
-        }}
-      </Fiora>
-    );
-  }
+function Login() {
+  return (
+    <Fiora
+      name="Login"
+      validate={async data => {
+        console.log(data);
+        const errors = {};
+        const feedbacks = {};
+        if (data.username.length < 5) {
+          errors.username = 'hum...need more than 5 characters';
+        } else {
+          feedbacks.username = 'A very good username';
+        }
+        if (data.email.indexOf('@') === -1) {
+          errors.email = 'hum...need a valid email';
+        } else {
+          feedbacks.email = 'A very good email';
+        }
+        return { errors, feedbacks };
+      }}
+      onSubmit={login}
+    >
+      {({ handleSubmit }) => (
+        <Aux>
+          <Field name="username">
+            {({ value, error, handleChange }) => {
+              console.log('render username Field');
+              return (
+                <Input
+                  type="text"
+                  value={value}
+                  error={error}
+                  onChange={event => {
+                    handleChange(event.target.value);
+                  }}
+                />
+              );
+            }}
+          </Field>
+          <Field name="email">
+            {({ value, error, handleChange }) => {
+              console.log('render email Field');
+              return (
+                <Input
+                  type="email"
+                  value={value}
+                  error={error}
+                  onChange={event => {
+                    handleChange(event.target.value);
+                  }}
+                />
+              );
+            }}
+          </Field>
+          <button onClick={handleSubmit}>Submit</button>
+        </Aux>
+      )}
+    </Fiora>
+  );
 }
 
 export default Login;
