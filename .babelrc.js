588eb714e76644b13b4b1520bf3a2ca60170a67f
Xiaofan Wu <xiaofan2406@gmail.com>
2017-09-29 01:03:35 +1000

    build fix

--- .babelrc.js
+++ .babelrc.js
@@ -8,6 +8,11 @@ module.exports = {
     'babel-plugin-transform-class-properties',
     ['babel-plugin-transform-object-rest-spread', { useBuiltIns: true }],
     ['babel-plugin-transform-react-jsx', { useBuiltIns: true }],
+    [
+      'babel-plugin-transform-runtime',
+      { helpers: false, polyfill: false, regenerator: true }
+    ],
+    ['babel-plugin-transform-regenerator', { async: false }],
     'babel-plugin-transform-export-extensions'
   ]
 };
