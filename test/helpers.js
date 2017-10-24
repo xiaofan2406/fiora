ab07d41048d594f629498cec8abf7eae3d6c8ae0
Xiaofan Wu <xiaofan2406@gmail.com>
2017-10-25 00:02:33 +1100

    update helper test

--- test/helpers.js
+++ test/helpers.js
@@ -4,3 +4,11 @@ test('getFormFieldKey returns the correct key name', () => {
   const key = helpers.getFormFieldKey('login', 'username');
   expect(key).toBe('username@login');
 });
+
+test('zip returns an object with first array being keys and second array beging values', () => {
+  expect(helpers.zip(['x', 'u'], [1, 2])).toEqual({ x: 1, u: 2 });
+  expect(helpers.zip(['x', 'u'], [1])).toEqual({ x: 1 });
+  expect(helpers.zip(['x', 'u'], [1, 2, 3])).toEqual({ x: 1, u: 2 });
+  expect(helpers.zip(['x'], [])).toEqual({});
+  expect(helpers.zip([], [1, 2])).toEqual({});
+});
