930b5bac1bb9e35f135ab6cb3ab74ec588f0d5f9
Xiaofan Wu <xiaofan2406@gmail.com>
2017-11-11 00:05:24 +1100

    add the new form selectors working with formmeta

--- src/FormMeta.js
+++ src/FormMeta.js
@@ -4,27 +4,38 @@ import { compose } from 'redux';
 import {
   getFormError,
   getIsFormValidating,
-  getIsFormSubmitting
+  getIsFormSubmitting,
+  getIsFormTouched,
+  getFormHasError
 } from './selectors';
 import withFiora from './withFiora';
 
 // the props passed in are flags to indicate whether to subscribe to the meta
 // the injected value for `children` render function are actual value in store
-function FormMeta({ children, error, isValidating, isSubmitting }) {
-  return children({ error, isValidating, isSubmitting });
+function FormMeta({
+  children,
+  error,
+  isValidating,
+  isSubmitting,
+  isTouched,
+  hasError
+}) {
+  return children({ error, isValidating, isSubmitting, isTouched, hasError });
 }
 
 FormMeta.propTypes = {
   children: PropTypes.func.isRequired,
   error: PropTypes.any,
   isValidating: PropTypes.bool,
-  isSubmitting: PropTypes.bool
+  isSubmitting: PropTypes.bool,
+  isTouched: PropTypes.bool,
+  hasError: PropTypes.bool
 };
 
 // props.formName is injected by `withFiora`
 export const mapState = (
   state,
-  { formName, error, isValidating, isSubmitting }
+  { formName, error, isValidating, isSubmitting, isTouched, hasError }
 ) => {
   const props = {};
   if (error) props.error = getFormError(state, { formName });
@@ -32,6 +43,8 @@ export const mapState = (
     props.isValidating = getIsFormValidating(state, { formName });
   if (isSubmitting)
     props.isSubmitting = getIsFormSubmitting(state, { formName });
+  if (isTouched) props.isTouched = getIsFormTouched(state, { formName });
+  if (hasError) props.hasError = getFormHasError(state, { formName });
   return props;
 };
 
--- test/FormMeta.js
+++ test/FormMeta.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import FormMeta, { Component } from '../src/FormMeta';
+import { getFormFieldKey } from '../src/helpers';
 
 const formName = 'login';
 
@@ -33,10 +34,19 @@ describe('enhanced FormMeta Component', () => {
             fiora: {
               forms: {
                 login: {
+                  fields: ['username'],
                   isSubmitting: false,
                   isValidating: true,
                   error: 'invalid',
                   ...storeState
+                }
+              },
+              fields: {
+                [getFormFieldKey(formName, 'username')]: {
+                  value: null,
+                  error: null,
+                  isTouched: false,
+                  isValidating: false
                 }
               }
             }
@@ -57,6 +67,8 @@ describe('enhanced FormMeta Component', () => {
     expect(wrapper.props().error).toBe('invalid');
     expect(wrapper.props()).not.toHaveProperty('isValidating');
     expect(wrapper.props()).not.toHaveProperty('isSubmitting');
+    expect(wrapper.props()).not.toHaveProperty('isTouched');
+    expect(wrapper.props()).not.toHaveProperty('hasError');
   });
 
   it('injects isValidating store state if subscribed', () => {
@@ -65,6 +77,8 @@ describe('enhanced FormMeta Component', () => {
     expect(wrapper.props().isValidating).toBe(true);
     expect(wrapper.props()).not.toHaveProperty('error');
     expect(wrapper.props()).not.toHaveProperty('isSubmitting');
+    expect(wrapper.props()).not.toHaveProperty('isTouched');
+    expect(wrapper.props()).not.toHaveProperty('hasError');
   });
 
   it('injects isSubmitting store state if subscribed', () => {
@@ -73,5 +87,27 @@ describe('enhanced FormMeta Component', () => {
     expect(wrapper.props().isSubmitting).toBe(false);
     expect(wrapper.props()).not.toHaveProperty('error');
     expect(wrapper.props()).not.toHaveProperty('isValidating');
+    expect(wrapper.props()).not.toHaveProperty('isTouched');
+    expect(wrapper.props()).not.toHaveProperty('hasError');
+  });
+
+  it('injects isTouched computed store state if subscribed', () => {
+    const props = { isTouched: true, children: jest.fn(() => '') };
+    const wrapper = mountWithState(props).find(Component);
+    expect(wrapper.props().isTouched).toBe(false);
+    expect(wrapper.props()).not.toHaveProperty('error');
+    expect(wrapper.props()).not.toHaveProperty('isValidating');
+    expect(wrapper.props()).not.toHaveProperty('isSubmitting');
+    expect(wrapper.props()).not.toHaveProperty('hasError');
+  });
+
+  it('injects hasError computed store state if subscribed', () => {
+    const props = { hasError: true, children: jest.fn(() => '') };
+    const wrapper = mountWithState(props).find(Component);
+    expect(wrapper.props().hasError).toBe(true);
+    expect(wrapper.props()).not.toHaveProperty('error');
+    expect(wrapper.props()).not.toHaveProperty('isValidating');
+    expect(wrapper.props()).not.toHaveProperty('isSubmitting');
+    expect(wrapper.props()).not.toHaveProperty('isTouched');
   });
 });
