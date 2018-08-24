1a9a01b319cd4eed7650e23f52923172b1de54d6
Xiaofan Wu <xiaofan2406@gmail.com>
2018-08-24 20:38:22 +1000

    move out profile form

--- docs/components/About.js
+++ docs/components/About.js
@@ -1,49 +1,19 @@
+/* @flow */
 import React from 'react';
 import { connect } from 'react-redux';
 import {
   updateProfile as updateProfileAction,
   getProfile,
 } from 'store/profile';
+import { Profile } from '../forms';
 
-import createForm from 'fiora/Fiora';
+type AboutProps = {
+  profile: {},
+  updateProfile: (profile: {}) => {},
+};
 
-const { Form, Field } = createForm('profile');
-
-const About = ({ updateProfile, profile }) => (
-  <Form onSubmit={updateProfile} initialValues={profile}>
-    <label htmlFor="name">
-      name:
-      <Field name="name">
-        {({ value, handleChange }) => (
-          <input
-            value={value}
-            onChange={event => {
-              handleChange(event.target.value);
-            }}
-          />
-        )}
-      </Field>
-    </label>
-    <br />
-    <label htmlFor="role">
-      role:
-      <Field name="role">
-        {({ value, error, handleChange, isTouched }) => (
-          <>
-            <input
-              value={value}
-              onChange={event => {
-                handleChange(event.target.value);
-              }}
-            />
-            {isTouched && error ? <div>{JSON.stringify(error)}</div> : null}
-          </>
-        )}
-      </Field>
-      <br />
-      <button type="submit">submit</button>
-    </label>
-  </Form>
+const About = ({ updateProfile, profile }: AboutProps) => (
+  <Profile updateProfile={updateProfile} profile={profile} />
 );
 
 const mapState = state => ({
